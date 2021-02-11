16259a2061a5281fa71f0b08aff214a20036ad4c
Alexandra Murawski <almurawski15@gmail.com>
2021-02-11 16:07:11 -0500

    Created variables and generated logic patterns

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+let generateBtn = document.querySelector("#generate");
+
+// Assigned Variables 
+let lowerLetters = "abcdefghijklmnopqrstuvwxyz";
+let upperLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let numbers = "0123456789";
+let specialChars = "!@,#$%&*{}[]/\\+=";
+let userchoice = "";
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  let password = generatePassword();
+  let passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
@@ -12,3 +19,53 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// Logic to randomly generate password based on parameters chosen
+function generatePassword() {
+
+	//Initiating random generation to produce password in the end result 
+	let result = "";
+	//Asks the user how many characters to include in their password 
+	let length = prompt("How many characters would you like to include in your password? (must be between 8 and 128)");
+	if(isNaN(length)){
+	  alert("You must input a number!");
+	  return generatePassword()
+	}
+	if(length<8 || length> 128){
+	  alert("Please choose a number between 8 - 128!");
+	  return generatePassword()
+	}	
+
+//Asks the user if they want to include the following variables upperLetters, lowerLetters, numbers, and special character?
+	let wantLowercase = confirm("Would you like to include lowercase letters?");
+	let wantUppercase = confirm("Would you like to include uppercase letters?");
+	let wantNumbers = confirm("Would you like to include numbers?");
+	let wantSpecial = confirm("Would you like to include special characters?");
+
+
+	  if(!wantLowercase&&!wantUppercase&&!wantNumbers&&!wantSpecial){
+		alert("You must choose at least 1 character type!");
+		return generatePassword()
+	  }
+	
+	  if(wantUppercase){
+		userchoice += upperLetters
+	  }
+	  if(wantLowercase){
+		userchoice += lowerLetters
+	  }
+	
+	  if(wantNumbers) {
+		userchoice += numbers
+	  }
+	
+	  if(wantSpecial) {
+		userchoice += specialChars
+	  }
+	
+	for (var i = 0; i < length; i++) {
+	  result += userchoice.charAt(Math.floor(Math.random() * userchoice.length));
+	}
+	return result;
+	
+	}
